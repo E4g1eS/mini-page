fd74014fbb396ef99332e31efa36bc85d380f01a
Richard Smely <smelyrich@gmail.com>
2025-08-05 22:03:34 +0200

    WIP signalling rework

--- react-app/src/components/Connection.tsx
+++ react-app/src/components/Connection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Connection.css";
 
-export const RTC_CONFIG: RTCConfiguration = {
+const RTC_CONFIG: RTCConfiguration = {
   iceServers: [
     {
       urls: "stun:relay.metered.ca:80",
@@ -27,6 +27,18 @@ export const RTC_CONFIG: RTCConfiguration = {
   ],
 };
 
+const RTC_API_URL = process.env.REACT_APP_WEBRTC_URL;
+
+function generateRandomString(length: number): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
 enum LogSeverity {
   DEBUG = "DEBUG",
   INFO = "INFO",
@@ -34,254 +46,214 @@ enum LogSeverity {
   ERROR = "ERROR",
 }
 
-function debugLog(text: string, severity: LogSeverity = LogSeverity.DEBUG) {
+function log(text: string, severity: LogSeverity = LogSeverity.DEBUG) {
   const logText = `[${new Date().toISOString()}] [${severity}] ${text}`;
-  console.log(logText);
-}
 
-// Info sent from one peer to another during the connection process
-// This is used to store the description and ICE candidates which is "local" for the sending peer and "remote" for the receiving peer
-class RemoteInfo {
-  description: RTCSessionDescriptionInit;
-  iceCandidates: RTCIceCandidate[];
-
-  constructor(
-    description: RTCSessionDescriptionInit,
-    iceCandidates: RTCIceCandidate[]
-  ) {
-    this.description = description;
-    this.iceCandidates = iceCandidates;
-  }
-
-  static parse(): RemoteInfo | null {
-    const textInput = document.getElementById("textInput") as HTMLInputElement;
-    if (!textInput) {
-      debugLog("Text input element not found.", LogSeverity.ERROR);
-      return null;
-    }
-
-    let remoteInfo: RemoteInfo | null = null;
-
-    try {
-      remoteInfo = JSON.parse(textInput.value) as RemoteInfo;
-    } catch (error) {
-      debugLog(`Error parsing remote info: ${error}`, LogSeverity.ERROR);
-      return null;
-    }
-
-    return remoteInfo;
+  switch (severity) {
+    case LogSeverity.DEBUG:
+      console.debug(logText);
+      break;
+    case LogSeverity.INFO:
+      console.info(logText);
+      break;
+    case LogSeverity.WARNING:
+      console.warn(logText);
+      break;
+    case LogSeverity.ERROR:
+      console.error(logText);
+      break;
+    default:
+      console.log(logText);
+      break;
   }
 }
 
 class Peer {
-  connection: RTCPeerConnection | null = null;
-  dataChannel: RTCDataChannel | null = null;
-  iceCandidates: RTCIceCandidate[] = [];
+  connection: RTCPeerConnection;
+  orderedData: RTCDataChannel | null = null;
+  unorderedData: RTCDataChannel | null = null;
 
-  constructor() {
+  localName: string;
+  remoteName: string;
+
+  constructor(localName: string, remoteName: string) {
+    this.localName = localName;
+    this.remoteName = remoteName;
+
     this.connection = new RTCPeerConnection(RTC_CONFIG);
     this.connection.addEventListener("connectionstatechange", () => {
-      debugLog(
+      log(
         `Connection state changed: ${this.connection?.connectionState}`,
         LogSeverity.INFO
       );
     });
-    this.connection.addEventListener("icecandidate", (event) => {
-      if (event.candidate) {
-        debugLog(
-          `New ICE candidate: ${JSON.stringify(event.candidate)}`,
-          LogSeverity.INFO
-        );
-        this.iceCandidates.push(event.candidate);
-      } else {
-        debugLog("All ICE candidates have been discovered.", LogSeverity.INFO);
-        this.printRemoteInfo();
-      }
-    });
-  }
-
-  printRemoteInfo() {
-    if (!this.connection) {
-      debugLog("Connection is not initialized.", LogSeverity.ERROR);
-      return;
-    }
-
-    if (!this.connection.localDescription) {
-      debugLog("Local description is not set.", LogSeverity.ERROR);
-      return;
-    }
-
-    const remoteInfo = new RemoteInfo(
-      this.connection.localDescription,
-      this.iceCandidates
+    this.connection.addEventListener(
+      "icecandidate",
+      this.iceCandidateDiscovered.bind(this)
     );
-    debugLog(`Remote info: ${JSON.stringify(remoteInfo)}`, LogSeverity.INFO);
   }
 
-  recieveRemoteInfo(remoteInfo: RemoteInfo) {
-    if (!this.connection) {
-      debugLog("Connection is not initialized.", LogSeverity.ERROR);
-      return;
-    }
-
-    if (remoteInfo.description) {
-      this.connection.setRemoteDescription(remoteInfo.description).then(
-        () => {
-          debugLog("Remote description set successfully.", LogSeverity.INFO);
-        },
-        (error) => {
-          debugLog(
-            `Error setting remote description: ${error}`,
-            LogSeverity.ERROR
-          );
-        }
+  iceCandidateDiscovered(iceEvent: RTCPeerConnectionIceEvent) {
+    if (iceEvent.candidate) {
+      log(
+        `New ICE candidate: ${JSON.stringify(iceEvent.candidate)}`,
+        LogSeverity.INFO
       );
-    }
-
-    remoteInfo.iceCandidates.forEach((candidate) => {
-      this.connection!.addIceCandidate(candidate).catch((error) => {
-        debugLog(`Error adding ICE candidate: ${error}`, LogSeverity.ERROR);
+      fetch(`${RTC_API_URL}/webrtc`, {
+        method: "POST",
+        body: JSON.stringify({
+          peerName: this.localName,
+          iceCandidate: iceEvent.candidate,
+        }),
+      }).catch((error) => {
+        log(`Error sending ICE candidate: ${error}`, LogSeverity.ERROR);
       });
-    });
+    } else {
+      log("All ICE candidates have been discovered.", LogSeverity.INFO);
+      fetch(`${RTC_API_URL}/webrtc`, {
+        method: "POST",
+        body: JSON.stringify({
+          peerName: this.localName,
+          iceCandidate: null,
+        }),
+      }).catch((error) => {
+        log(`Error ICE candidate stop: ${error}`, LogSeverity.ERROR);
+      });
+    }
   }
 }
 
 class Host extends Peer {
-  constructor() {
-    super();
+  constructor(hostName: string) {
+    super(hostName);
 
-    if (!this.connection) {
-      throw new Error("Connection is not initialized.");
-    }
-
-    this.dataChannel = this.connection.createDataChannel("orderedData", {
+    this.orderedData = this.connection.createDataChannel("orderedData", {
       ordered: true,
     });
-    this.dataChannel.addEventListener("open", () => {
-      debugLog(
-        "Data channel is open. Ready to send messages.",
+    this.orderedData.addEventListener("open", () => {
+      log(
+        "Ordered data channel is open. Ready to send messages.",
         LogSeverity.INFO
       );
-      this.dataChannel!.send("Hello from the host!");
+      this.orderedData!.send("Hello from the host!");
+    });
+
+    this.unorderedData = this.connection.createDataChannel("unorderedData", {
+      ordered: false,
+    });
+    this.unorderedData.addEventListener("open", () => {
+      log(
+        "Unordered data channel is open. Ready to send messages.",
+        LogSeverity.INFO
+      );
+      this.unorderedData!.send("Hello from the host (unordered)!");
     });
 
     this.connection.createOffer().then(
       (offer) => {
         this.connection!.setLocalDescription(offer).then(
           () => {
-            debugLog("Local description set successfully.", LogSeverity.INFO);
+            log("Local description set successfully.", LogSeverity.INFO);
           },
           (error) => {
-            debugLog(
-              `Error setting local description: ${error}`,
-              LogSeverity.ERROR
-            );
+            log(`Error setting local description: ${error}`, LogSeverity.ERROR);
           }
         );
 
         const offerString = JSON.stringify(offer);
-        debugLog(`Created offer: ${offerString}`, LogSeverity.INFO);
+        log(`Created offer: ${offerString}`, LogSeverity.INFO);
       },
       (error) => {
-        debugLog(`Error creating offer: ${error}`, LogSeverity.ERROR);
+        log(`Error creating offer: ${error}`, LogSeverity.ERROR);
       }
     );
   }
 }
 
 class Client extends Peer {
-  constructor(remoteInfo: RemoteInfo) {
-    super();
-
-    if (!this.connection) {
-      throw new Error("Connection is not initialized.");
-    }
+  constructor(hostName: string) {
+    super(hostName);
 
     this.connection.setRemoteDescription(remoteInfo.description).then(
       () => {
-        debugLog("Remote description set successfully.", LogSeverity.INFO);
+        log("Remote description set successfully.", LogSeverity.INFO);
       },
       (error) => {
-        debugLog(
-          `Error setting remote description: ${error}`,
-          LogSeverity.ERROR
-        );
+        log(`Error setting remote description: ${error}`, LogSeverity.ERROR);
       }
     );
 
     remoteInfo.iceCandidates.forEach((candidate) => {
       this.connection!.addIceCandidate(candidate).catch((error) => {
-        debugLog(`Error adding ICE candidate: ${error}`, LogSeverity.ERROR);
+        log(`Error adding ICE candidate: ${error}`, LogSeverity.ERROR);
       });
     });
 
     this.connection.createAnswer().then(
       (answer) => {
         this.connection!.setLocalDescription(answer).catch((error) => {
-          debugLog(
-            `Error setting local description: ${error}`,
-            LogSeverity.ERROR
-          );
+          log(`Error setting local description: ${error}`, LogSeverity.ERROR);
         });
 
         const answerString = JSON.stringify(answer);
-        debugLog(`Created answer: ${answerString}`, LogSeverity.INFO);
+        log(`Created answer: ${answerString}`, LogSeverity.INFO);
       },
       (error) => {
-        debugLog(`Error creating answer: ${error}`, LogSeverity.ERROR);
+        log(`Error creating answer: ${error}`, LogSeverity.ERROR);
       }
     );
 
     this.connection.addEventListener("datachannel", (event) => {
-      debugLog(
+      log(
         `Data channel received from host: ${event.channel.label}`,
         LogSeverity.INFO
       );
-      this.dataChannel = event.channel;
-      this.dataChannel.addEventListener("message", (event) => {
-        debugLog(`Received message: ${event.data}`, LogSeverity.INFO);
+      this.orderedData = event.channel;
+      this.orderedData.addEventListener("message", (event) => {
+        log(`Received message: ${event.data}`, LogSeverity.INFO);
       });
     });
   }
 }
 
-let peer: Host | Client | null = null;
-
-function createHost() {
-  if (peer) {
-    debugLog("Peer already exists.", LogSeverity.WARNING);
-    return;
+function getServerName(): string {
+  const serverNameInput = document.getElementById("serverNameInput");
+  if (!serverNameInput || !(serverNameInput instanceof HTMLInputElement)) {
+    log("Server name input not found.", LogSeverity.ERROR);
+    return "";
   }
 
-  peer = new Host();
-}
+  if (!serverNameInput.value.trim())
+    serverNameInput.value = generateRandomString(8);
 
-function addRemoteInfo() {
-  const remoteInfo = RemoteInfo.parse();
-  if (!remoteInfo) {
-    debugLog("Remote info is null or invalid.", LogSeverity.ERROR);
-    return;
-  }
-
-  if (!peer) {
-    // It is assumed that this is the client side and host already sent the remote description
-    peer = new Client(remoteInfo);
-  } else if (peer instanceof Host) {
-    // If the peer is a host, we assume it is waiting for a client to connect
-    peer.recieveRemoteInfo(remoteInfo);
-  }
+  return serverNameInput.value.trim();
 }
 
 function Connection() {
+  const [peer, setPeer] = React.useState<Host | Client | null>(null);
+
   return (
     <div className="Connection">
-      <button onClick={() => createHost()}>Create Host</button>
       <input
-        id="textInput"
+        id="serverNameInput"
         type="text"
-        placeholder="Enter Remote Description 2"
+        placeholder="Server Name"
+        disabled={peer !== null}
       ></input>
-      <button onClick={() => addRemoteInfo()}>Add Remote Info</button>
+      <button
+        onClick={() => setPeer(new Host(getServerName()))}
+        id="btnCreateServer"
+        disabled={peer !== null}
+      >
+        Create Server
+      </button>
+      <button
+        onClick={() => setPeer(new Client(getServerName()))}
+        id="btnJoinServer"
+        disabled={peer !== null}
+      >
+        Join Server
+      </button>
     </div>
   );
 }
--- react-app/src/components/Game.tsx
+++ react-app/src/components/Game.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import './Game.css';
+import React from "react";
+import "./Game.css";
 
 function Game() {
   return (
     <div className="Game">
+      <canvas id="gameCanvas" className="gameCanvas"></canvas>
     </div>
   );
 }
