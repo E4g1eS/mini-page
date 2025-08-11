49506e05ac1e09f242e88ee9f13da1723750af47
Richard Smely <smelyrich@gmail.com>
2025-08-11 15:14:20 +0200

    WIP before testing

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+from enum import Enum, IntEnum
 import json
 
 app = FastAPI()
@@ -10,6 +12,14 @@ async def root():
 @app.get("/health")
 async def health_check():
     return {"status": "healthy"}
+
+def return_error(message: str):
+    return {"success": False, "error": message}
+
+def return_success(data: dict | None = None):
+    if data is None:
+        data = {}
+    return {"success": True, **data}
 
 class Connection:
     host_name: str
@@ -23,7 +33,7 @@ class Connection:
     def __init__(self, host_name: str, offer: dict):
         self.host_name = host_name
         self.offer = offer
-        self.client_name = generate_client_name(host_name)
+        self.client_name = self.generate_client_name(host_name)
 
     def generate_client_name(self) -> str:
         """
@@ -33,94 +43,89 @@ class Connection:
 
 CONNECTIONS: list[Connection] = list()
 
+class MessageType(str, Enum):
+    offer = 'offer'
+    answer = 'answer'
+    ice_candidate = "candidate"
+
+class WebRTCData(BaseModel):
+    messageType: MessageType
+    peerName: str
+    payload: dict
+
 @app.post("/webrtc")
-async def webrtc_handler(data: dict):
+async def webrtc_handler(data: WebRTCData):
 
     print("Received WebRTC data:", data)
 
-    message_type = data.get("messageType")
-    assert message_type is not None, "Missing 'messageType' field in WebRTC data"
-
-    peer_name = data.get("peerName")
-    assert host_name is not None, "Missing 'peerName' field in WebRTC data"
-
-    match message_type:
-        case "offer":
-            offer = data.get("offer")
-            assert offer is not None, "Missing 'offer' field in WebRTC data"
+    match data.messageType.value:
+        case MessageType.offer:
             connection = Connection(
-                host_name=peer_name,
-                offer=offer
+                host_name=data.peerName,
+                offer=data.payload
             )
             CONNECTIONS.append(connection)
-            return {"success": True, "clientName": connection.client_name}
+            return return_success({"clientName": connection.client_name})
 
-        case "answer":
-            answer = data.get("answer")
-            assert answer is not None, "Missing 'answer' field in WebRTC data"
-            
+        case MessageType.answer:
             for connection in CONNECTIONS:
-                if connection.host_name == peer_name:
-                    connection.answer = answer
-                    return {"success": True}
+                if connection.host_name == data.peerName:
+                    connection.answer = data.payload
+                    return return_success()
 
-            return {"success": False, "error": "Host not found"}
+            return return_error("Host not found")
 
-        case "candidate":
-            ice_candidate = data.get("candidate")
-            assert ice_candidate is not None, "Missing 'candidate' field in WebRTC data"
-
+        case MessageType.ice_candidate:
             # ICE candidates are set for self --> so either client name or host name
-
             for connection in CONNECTIONS:
-                if connection.host_name == peer_name:
-                    connection.host_ice_candidates.append(ice_candidate)
-                    return {"success": True}
-                elif connection.client_name == peer_name:
-                    connection.client_ice_candidates.append(ice_candidate)
-                    return {"success": True}
+                if connection.host_name == data.peerName:
+                    connection.host_ice_candidates.append(data.payload)
+                    return return_success()
+                elif connection.client_name == data.peerName:
+                    connection.client_ice_candidates.append(data.payload)
+                    return return_success()
 
         case _:
-            return {"success": False, "error": "Unknown WebRTC message type"}
+            return return_error("Unknown message type")
 
-    return {"success": False}
+    # This should never happen
+    return return_error("Unknown message type")
 
 @app.get("/webrtc/ice_candidates")
-async def get_ice_candidates(data: dict):
-    # Either from host or client
-    peer_name = data.get("peerName")
-    assert peer_name is not None, "Missing 'peerName' field in WebRTC data"
+async def get_ice_candidates(peer_name: str = Header(None)):
+    if peer_name is None:
+        return return_error("Missing 'peer-name' header in WebRTC request")
 
     for connection in CONNECTIONS:
         if connection.host_name == peer_name:
             ice_candidates = connection.host_ice_candidates
             connection.host_ice_candidates = list()  # Clear after fetching
-            return {"success": True, "iceCandidates": ice_candidates}
+            return return_success({"iceCandidates": ice_candidates})
         elif connection.client_name == peer_name:
             ice_candidates = connection.client_ice_candidates
             connection.client_ice_candidates = list()  # Clear after fetching
-            return {"success": True, "iceCandidates": ice_candidates}
+            return return_success({"iceCandidates": ice_candidates})
 
-    return {"sucess": False, "error": "Peer not found"}
+    return return_error("Peer not found")
 
 @app.get("/webrtc/offer")
-async def get_offer(data: dict):
-    peer_name = data.get("peerName")
-    assert peer_name is not None, "Missing 'peerName' field in WebRTC data"
+async def get_offer(peer_name: str = Header(None)):
+    if peer_name is None:
+        return return_error("Missing 'peer-name' header in WebRTC request")
 
     for connection in CONNECTIONS:
         if connection.host_name == peer_name:
-            return {"success": True, "offer": connection.offer}
+            return return_success({"offer": connection.offer, "clientName": connection.client_name})
 
-    return {"success": False, "error": "Peer not found"}
+    return return_error("Host not found")
 
 @app.get("/webrtc/answer")
-async def get_answer(data: dict):
-    peer_name = data.get("peerName")
-    assert peer_name is not None, "Missing 'peerName' field in WebRTC data"
+async def get_answer(peer_name: str = Header(None)):
+    if peer_name is None:
+        return return_error("Missing 'peer-name' header in WebRTC request")
 
     for connection in CONNECTIONS:
         if connection.client_name == peer_name:
-            return {"success": True, "answer": connection.answer}
+            return return_success({"answer": connection.answer})
 
-    return {"success": False, "error": "Peer not found"}
+    return return_error("Client not found")
