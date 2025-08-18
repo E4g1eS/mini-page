3f0035d261787fe4a70892f65a9bab29ae0b4438
Richard Smely <smelyrich@gmail.com>
2025-08-18 17:14:44 +0200

    Render pong

--- react-app/src/func/renderer.ts
+++ react-app/src/func/renderer.ts
@@ -1,5 +1,6 @@
 import { Pong } from "./pong";
 import { createScopedLog, LogSeverity } from "./logging";
+import * as QM from "./math";
 
 // This renders the game
 
@@ -25,25 +26,77 @@ export class Renderer {
     this.stopped = true;
   }
 
-  tmpTest = 0;
-
   private renderFrame() {
     if (!this.pong || !this.ctx)
       throw new Error("Renderer is not initialized.");
 
     const startTime = performance.now();
 
-    this.tmpTest = (this.tmpTest + 1) % this.ctx.canvas.width;
-
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-    this.ctx.beginPath();
-    this.ctx.moveTo(this.tmpTest, 0);
-    this.ctx.lineTo(this.ctx.canvas.width, this.ctx.canvas.height);
-    this.ctx.stroke();
+    this.renderPong();
 
     const endTime = performance.now();
     log(`Frame rendered in ${endTime - startTime} ms.`, LogSeverity.VERBOSE);
 
     requestAnimationFrame(this.renderFrame.bind(this));
+  }
+
+  /**
+   * Converts relative size (0-1 in respect to canvas size) into absolute pixel size.
+   * Assumes square canvas.
+   */
+  private convertToAbsolute(relativeSize: number) {
+    const canvasSize = this.ctx!.canvas.width;
+    return relativeSize * canvasSize;
+  }
+
+  private renderPong() {
+    const pong = this.pong!;
+    const ctx = this.ctx!;
+
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(ctx.canvas.width, ctx.canvas.height);
+    ctx.stroke();
+
+    // Draw players
+    const HALF_PADDLE_SIZE = new QM.Vec2(
+      pong.PADDLE_SIZE.x / 2,
+      pong.PADDLE_SIZE.y / 2
+    );
+
+    // Draw left player
+    const leftCenter = new QM.Vec2(
+      0.5 - pong.DISTANCE_BETWEEN_PLAYERS / 2 - pong.PADDLE_SIZE.x / 2,
+      pong.leftPlayer.yPosition
+    );
+
+    ctx.fillRect(
+      this.convertToAbsolute(leftCenter.x - HALF_PADDLE_SIZE.x),
+      this.convertToAbsolute(leftCenter.y - HALF_PADDLE_SIZE.y),
+      this.convertToAbsolute(pong.PADDLE_SIZE.x),
+      this.convertToAbsolute(pong.PADDLE_SIZE.y)
+    );
+
+    // Draw right player
+    const rightCenter = new QM.Vec2(
+      0.5 + pong.DISTANCE_BETWEEN_PLAYERS / 2 + pong.PADDLE_SIZE.x / 2,
+      pong.rightPlayer.yPosition
+    );
+
+    ctx.fillRect(
+      this.convertToAbsolute(rightCenter.x - HALF_PADDLE_SIZE.x),
+      this.convertToAbsolute(rightCenter.y - HALF_PADDLE_SIZE.y),
+      this.convertToAbsolute(pong.PADDLE_SIZE.x),
+      this.convertToAbsolute(pong.PADDLE_SIZE.y)
+    );
+
+    // Draw ball
+    ctx.fillRect(
+      this.convertToAbsolute(pong.ball.position.x - pong.BALL_SIZE / 2),
+      this.convertToAbsolute(pong.ball.position.y - pong.BALL_SIZE / 2),
+      this.convertToAbsolute(pong.BALL_SIZE),
+      this.convertToAbsolute(pong.BALL_SIZE)
+    );
   }
 }
