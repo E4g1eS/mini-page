ed596db2baa327348a7166aaf245cdfd7bc5fd1f
Richard Smely <smelyrich@gmail.com>
2025-08-18 22:34:06 +0200

    WIP

--- react-app/src/func/math.ts
+++ react-app/src/func/math.ts
@@ -14,4 +14,20 @@ export class Vec2 {
   static zero() {
     return new Vec2(0, 0);
   }
+
+  static plus(vecA: Vec2, vecB: Vec2) {
+    return new Vec2(vecA.x + vecB.x, vecA.y + vecB.y);
+  }
+
+  static minus(vecA: Vec2, vecB: Vec2) {
+    return new Vec2(vecA.x - vecB.x, vecA.y - vecB.y);
+  }
+
+  static negative(vec: Vec2) {
+    return new Vec2(-vec.x, -vec.y);
+  }
+
+  static scale(vec: Vec2, scalar: number) {
+    return new Vec2(vec.x * scalar, vec.y * scalar);
+  }
 }
